02aee56c82cb971035321fb78f048ae952427101
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-16 16:09:53 -0700

    added a bunch of controllers, routes, view pages)

---
+++ app/controllers/animals1_controller.rb
@@ -0,0 +1,47 @@
+class AnimalsController < ApplicationController
+
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      flash[:notice] = "This is a test for success"
+      redirect_to @animal
+    end
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      flash[:notice] = "This is a test for updates"
+      redirect_to @animal
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    animal = Animal.find(params[:id])
+    animal.destroy
+    redirect_to animals_path
+  end
+
+  def animal_params
+    params.require(:animal).permit(:name, :type, :age)
+  end
+
+end
